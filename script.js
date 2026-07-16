84934a8661cc495158a6e04d2fa1b6e86ce4869f
andino810-stack <andino810@gmail.com>
2026-07-15 22:45:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("formReserva").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const nombre = document.getElementById("nombre").value;
+  const servicio = document.getElementById("servicio").value;
+  const hora = document.getElementById("hora").value;
+
+  const mensaje = `Hola, soy ${nombre}. Quiero reservar un turno para ${servicio} a las ${hora}.`;
+  const url = `https://wa.me/5493765372920?text=${encodeURIComponent(mensaje)}`;
+
+  window.open(url, "_blank");
+});
+
+// Compartir aplicación
+document.getElementById("compartirApp").addEventListener("click", async () => {
+  const enlace = "https://mega.nz/folder/RNRT2ZhD#bzyGujwPRLca2N52WOL9gQ";
+
+  if (navigator.share) {
+    await navigator.share({
+      title: "Elevate Hair Studio",
+      text: "Descargá nuestra aplicación",
+      url: enlace
+    });
+  } else {
+    window.open(enlace, "_blank");
+  }
+});
